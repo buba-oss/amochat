59348fe13dfcd67a65e41cdfc7c25ef5b6924923
Buba Sanneh <sannehbalamo@gmail.com>
2022-03-22 23:45:30 +0100

    made changes in Main

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,19 @@ import 'package:amochat/screens/registration_screen.dart';
 import 'package:amochat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const AmoChat());
-
-
-
+void main() => runApp(AmoChat());
 
 class AmoChat extends StatelessWidget {
-  const AmoChat({Key? key}) : super(key: key);
-
-
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    initialRoute:  WelcomeScreen.id,
+      initialRoute: WelcomeScreen.id,
       routes: {
-       WelcomeScreen.id:(context) => WelcomeScreen(),
-       RegistrationScreen.id:(context) => RegistrationScreen(),
-       LoginScreen.id:(context) => LoginScreen(),
-        ChatScreen.id:(context) => ChatScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
       },
     );
   }
