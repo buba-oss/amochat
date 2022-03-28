b9d72396dc44b0668d3b2aded5ae5a6555f7fa17
Buba Sanneh <sannehbalamo@gmail.com>
2022-03-28 14:17:37 +0200

    add changes in Round Button

--- lib/components/rounded_button.dart
+++ lib/components/rounded_button.dart
@@ -17,8 +17,7 @@ class RoundedButton extends StatelessWidget {
         color: colour,
         borderRadius: BorderRadius.circular(30.0),
         child: MaterialButton(
-          onPressed: () {
-            onPressed;
+          onPressed:() {onPressed();
           },
           minWidth: 200.0,
           height: 42.0,
